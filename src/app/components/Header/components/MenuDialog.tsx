b27290c2264f9ace4c168245ca3224cd4b00a73c
Narry10 <harrywork.dlc@gmail.com>
2024-03-16 11:58:49 +0700

    Add new routes and components

--- src/app/components/Header/components/MenuDialog.tsx
+++ src/app/components/Header/components/MenuDialog.tsx
@@ -3,6 +3,24 @@ import AppsIcon from "@mui/icons-material/Apps";
 import Popover from "@mui/material/Popover";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import HomeIcon from "@mui/icons-material/Home";
+import Link from "next/link";
+import BlurOnIcon from "@mui/icons-material/BlurOn";
+import AddIcon from "@mui/icons-material/Add";
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import LinkIcon from "@mui/icons-material/Link";
+
+const menuItems = [
+  { href: "/", icon: <HomeIcon /> },
+  { href: "/resume-builder", icon: <StickyNote2Icon /> },
+  { href: "/resume-import", icon: <AddIcon /> },
+  { href: "/", icon: <AccountCircleIcon /> },
+  { href: "/", icon: <FormatListBulletedIcon /> },
+  { href: "/", icon: <LinkIcon /> },
+  // Add more items as needed
+];
 
 const MenuDialog = () => {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
@@ -35,7 +53,7 @@ const MenuDialog = () => {
   return (
     <div aria-describedby={id}>
       <IconButton onClick={handleClick}>
-        <AppsIcon />
+        <BlurOnIcon />
       </IconButton>
       <Popover
         id={id}
@@ -51,7 +69,17 @@ const MenuDialog = () => {
           horizontal: "center",
         }}
       >
-        <Typography sx={{ p: 2 }}>Nội dung của Popover.</Typography>
+        <div className="grid grid-cols-3 gap-8 p-4">
+          {menuItems.map(({ href, icon }, index) => (
+            <Link
+              key={index}
+              href={href}
+              className="flex h-[40px]  w-[40px] items-center justify-center p-2 text-3xl transition-all  "
+            >
+              {icon}
+            </Link>
+          ))}
+        </div>
       </Popover>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,26 @@
 "use client";
-import { Hero } from "home/Hero";
-import { useEffect, useState } from "react";
+import DefauftLayout from "./layout/DefauftLayout";
+
 import { useAppDispatch, useAppSelector } from "../contexts/hooks";
-import { authLoginWithEmail } from "../contexts/auth";
-import DefauftLayout from "layout/DefauftLayout";
-import { Steps } from "home/Steps";
-import { Features } from "home/Features";
-import { Testimonials } from "home/Testimonials";
+import { Hero } from "./home/Hero";
 
 export default function Home() {
   const dispatch = useAppDispatch();
   const user = useAppSelector((s) => s.user.data);
 
-  useEffect(() => {
-    dispatch(
-      authLoginWithEmail({
-        email: "dominhnhat.dlc@gmail.com",
-        password: "dominhnhat.dlc@gmail.com",
-      })
-    );
-  }, []);
+  // useEffect(() => {
+  //   dispatch(
+  //     authLoginWithEmail({
+  //       email: "dominhnhat.dlc@gmail.com",
+  //       password: "dominhnhat.dlc@gmail.com",
+  //     })
+  //   );
+  // }, []);
 
   return (
     <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12">
       <DefauftLayout>
         <Hero />
-        <Steps/>
-        <Features />
       </DefauftLayout>
     </main>
   );
